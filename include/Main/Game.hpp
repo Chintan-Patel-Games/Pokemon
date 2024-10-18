92895cb77cecb5f762b672a6d08318794ab1f3f0
ChintanPatel024 <chintankp15@gmail.com>
2024-10-18 10:24:36 +0530

    Added Pointers to whole code

--- include/Main/Game.hpp
+++ include/Main/Game.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "../include/Battle/BattleManager.hpp"
+#include "../include/Battle/WildEncounterManager.hpp"
 #include "../include/Character/Player/Player.hpp"
 #include "../include/Pokemon/Grass.hpp"
 
@@ -10,11 +12,15 @@ namespace N_Main
     class Game
     {
     private:
-        Grass forestGrass;
+        Grass* forestGrass = nullptr;
+        N_Battle::BattleManager* battleManager = nullptr; // Pointer to BattleManager
+        N_Battle::WildEncounterManager* encounterManager = nullptr; // Pointer to WildEncounterManager
+        N_Pokemon::Pokemon* wildPokemon = nullptr; // Pointer for Pokemon
 
     public:
         Game();
-        void GameLoop(Player &player);
-        void VisitPokeCenter(Player &player);
+        ~Game();
+        void GameLoop(Player* &player);
+        void VisitPokeCenter(Player* &player);
     };
 }
--- src/Character/Player/Player.cpp
+++ src/Character/Player/Player.cpp
@@ -16,13 +16,18 @@ namespace N_Character
         Player::Player()
         {
             name = "Trainer";
-            chosenPokemon = Pokemon(); // Using the default Pokemon constructor
+            chosenPokemon = new Pokemon(); // Using the default Pokemon constructor
         }
 
-        Player::Player(std::string p_name, Pokemon p_chosenPokemon)
+        Player::Player(std::string p_name, Pokemon* p_chosenPokemon)
         {
             name = p_name;
             chosenPokemon = p_chosenPokemon;
+        }
+
+        Player::~Player()
+        {
+            delete chosenPokemon;
         }
 
         void Player::ChoosePokemon(int choice)
@@ -30,19 +35,19 @@ namespace N_Character
             switch ((PokemonChoice)choice)
             {
             case PokemonChoice::Charmander:
-                chosenPokemon = Pokemon("Charmander", PokemonType::Fire, 100, 20);
+                chosenPokemon = new Pokemon("Charmander", PokemonType::Fire, 100, 20);
                 break;
             case PokemonChoice::Bulbasaur:
-                chosenPokemon = Pokemon("Bulbasaur", PokemonType::Grass, 100, 20);
+                chosenPokemon = new Pokemon("Bulbasaur", PokemonType::Grass, 100, 20);
                 break;
             case PokemonChoice::Squirtle:
-                chosenPokemon = Pokemon("Squirtle", PokemonType::Water, 100, 20);
+                chosenPokemon = new Pokemon("Squirtle", PokemonType::Water, 100, 20);
                 break;
             default:
-                chosenPokemon = Pokemon("Pikachu", PokemonType::Electric, 100, 20);
+                chosenPokemon = new Pokemon("Pikachu", PokemonType::Electric, 100, 20);
                 break;
             }
-            std::cout << "Player " << name << " chose " << chosenPokemon.GetPokemonName() << "!\n";
+            std::cout << "Player " << name << " chose " << chosenPokemon->GetPokemonName() << "!\n";
             Utility::WaitForEnter(); // Wait for user to press Enter before proceeding
         }
     }
